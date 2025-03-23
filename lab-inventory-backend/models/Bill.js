7287ea6ce4ee5ce4ff41a3e25e8020b2e854628a
KshitijDale <kshitijdale17@gmail.com>
2025-03-23 19:59:03 +0530

    nothing serious

--- lab-inventory-backend/models/Bill.js
+++ lab-inventory-backend/models/Bill.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
 const billSchema = new mongoose.Schema({
-    vendor: { type: mongoose.Schema.Types.ObjectId, ref: 'Vendor', required: true },
-    bill_date: { type: Date, required: true },
-    bill_record_ts: { type: Date, default: Date.now },
-    bill_amt: { type: Number, required: true },
-    added_by: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
-  });
+  vendor: { type: mongoose.Schema.Types.ObjectId, ref: 'Vendor', required: true },
+  bill_date: { type: Date, required: true },
+  bill_record_ts: { type: Date, default: Date.now },
+  bill_amt: { type: Number, required: true },
+  added_by: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
+});
 
 module.exports = mongoose.model('Bill', billSchema);  
